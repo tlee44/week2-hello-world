7d64bdd4e27fe0b0f59a3dfc3dca5d55fcbc25b8
Tyler Lee <tlee44@mail.valenciacollege.edu>
2016-02-24 16:48:00 -0500

    deleted excess lines
    deleted lines 10-12 and the exit on 20

--- helloworld.py
+++ helloworld.py
@@ -7,9 +7,6 @@ print '3. German'
 # Lines 2-6 give the user a welcome and display options
 lang = input()
 #Line 8 asks for input and sets a variable to represent input
-1 == ('Hello World')
-2 == ('Hola Mundo')
-3 == ('Hallo Welt')
 if lang == ('1'):
   print('Hello World')
 elif lang == ('2'):
@@ -17,6 +14,5 @@ elif lang == ('2'):
 else:
   print('Hallo Welt')
   #Lines 10-18 describe the various inputs possible and results
-exit()
 
 # make sure that your code contains comments explaining your logic!
