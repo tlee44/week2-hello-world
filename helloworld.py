e4f53deccd1bf85313d06310362971afd3fc5bd8
Tyler Lee <tlee44@mail.valenciacollege.edu>
2016-02-24 15:53:53 -0500

    Hello World
    Welcomes and asks the user for input to identify the correct language of
    the user and responds accordingly then exits. HOPEFULLY!

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,22 @@
-# replace the contents of this comment with your full name
-
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+#Tyler Lee
+welcome = 'Hello please select language'
+print welcome
+print '1. English'
+print '2. Spanish'
+print '3. German'
+# Lines 2-6 give the user a welcome and display options
+lang = input()
+#Line 8 asks for input and sets a variable to represent input
+1 == ('Hello World')
+2 == ('Hola Mundo')
+3 == ('Hallo Welt')
+if lang == ('1'):
+  print('Hello World')
+elif lang == ('2'):
+  print('Hola Mundo')
+else:
+  print('Hallo Welt')
+  #Lines 10-18 describe the various inputs possible and results
+exit()
 
 # make sure that your code contains comments explaining your logic!
-
